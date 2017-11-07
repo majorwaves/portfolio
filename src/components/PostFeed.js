86bcb6b37090eec10752b265bcadc0e3862fb536
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2017-11-06 18:51:13 -0800

    fixed api for link

--- src/components/PostFeed.js
+++ src/components/PostFeed.js
@@ -15,7 +15,7 @@ class PostFeed extends Component {
           <div className="postInfo">
             <h1>{project.fields.title}</h1>
             <span>
-            <a target="_blank" href={project.fields.link}>{project.fields.link}</a>
+            {project.fields.link ? <a target="_blank" href={project.fields.link}>{project.fields.link}</a> : ''}
             </span>
             <ReactMarkdown source={description} />
           </div>
