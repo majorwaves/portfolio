5a993b3c255897e9a2cadb7265ac438bf05adf7f
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2018-10-18 09:04:19 -0700

    Updated to CRA v2

---
+++ build/precache-manifest.b96d837663466517bb3e65144f17e67a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e08d89af73ba8d3d58f41fd2593e7d5e",
+    "url": "/static/media/web-buttons.e08d89af.svg"
+  },
+  {
+    "revision": "23bd79fbb1a7b81d1af542cb3beab5a2",
+    "url": "/static/media/web-buttons-night.23bd79fb.svg"
+  },
+  {
+    "revision": "8aa7c2f3f99bc1a4856ab605dcd23e16",
+    "url": "/static/media/HaasGrotDisp-55Roman.8aa7c2f3.ttf"
+  },
+  {
+    "revision": "5f3bc85405f25ad4fb9dbce560f91041",
+    "url": "/static/media/HaasGrotDisp-55Roman.5f3bc854.woff"
+  },
+  {
+    "revision": "554019b9dc26466a3c8fed3a4bded8e9",
+    "url": "/static/media/HaasGrotDisp-55Roman.554019b9.eot"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "01771c7cb2ef01904f15",
+    "url": "/static/js/main.01771c7c.chunk.js"
+  },
+  {
+    "revision": "847b0773a470c519be91",
+    "url": "/static/js/1.847b0773.chunk.js"
+  },
+  {
+    "revision": "01771c7cb2ef01904f15",
+    "url": "/static/css/main.dd95eb68.chunk.css"
+  },
+  {
+    "revision": "75b96423c376102c011f8f06179e6bb9",
+    "url": "/index.html"
+  }
+];
